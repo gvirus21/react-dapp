3500ceffe2132641d42d5d29d8e5c258b9234470
gourav <gvirus21yt@gmail.com>
2022-02-10 23:16:19 +0530

    hotfix - removed import hardhat and compiled

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-const { ethers } = require("hardhat");
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -9,12 +8,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.9",
   paths: {
