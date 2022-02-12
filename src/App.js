b9be3d8354884b82582ab300eb972c64b7058dea
gourav <gvirus21yt@gmail.com>
2022-02-12 11:53:16 +0530

    updated frontend for token.sol

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import { ethers } from "ethers";
 import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+import Token from "./artifacts/contracts/Token.sol/Token.json";
 
 const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const tokenAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 function App() {
   const [greeting, setGreetingValue] = useState("");
+  const [userAccount, setUserAccount] = useState("");
+  const [amount, setAmount] = useState(0);
 
-  const requestAccount = async () => {};
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  };
+
+  const getBalance = async () => {
+    if (typeof window.ethereum != "undefined") {
+      const [account] = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(tokenAddress, Token.abi, provider);
+      const balance = await contract.balanceOf(account);
+      console.log("Balance: ", balance.toString());
+    }
+  };
+
+  const sendCoins = async () => {
+    if (typeof window.ethereum != "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(tokenAddress, Token.abi, signer);
+      const transaction = await contract.transfer(userAccount, amount);
+      await transaction.wait();
+      console.log(`${amount} Coins Successfully sent to ${userAccount}`);
+    }
+  };
+
   const fetchGreeting = async () => {
     if (typeof window.ethereum != "undefined") {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -52,6 +82,17 @@ function App() {
           onChange={(e) => setGreetingValue(e.target.value)}
           placeholder="Set greeting"
           value={greeting}
+        />
+        <br />
+        <button onClick={getBalance}> Get Balance </button>
+        <button onClick={sendCoins}> Send Coins </button>
+        <input
+          onChange={(e) => setUserAccount(e.target.value)}
+          placeholder="Account Id"
+        />
+        <input
+          onChange={(e) => setAmount(e.target.value)}
+          placeholder="Amount"
         />
       </header>
     </div>
